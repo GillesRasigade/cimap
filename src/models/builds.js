ceb148a80443c41e99575009b9fec4ec2e47b4f6
Gilles <gilles.rasigade+github@gmail.com>
2017-09-04 22:59:35 +0200

    Add dependency definition

--- src/models/builds.js
+++ src/models/builds.js
@@ -82,16 +82,61 @@ export function findByName(name) {
   return db.builds.findOneAsync({ name });
 }
 
-export async function addDependency(_id, name) {
-  const dependencies = [];
-  const build = await get(_id);
-  console.log(build);
+/**
+ * Return deep dependencies
+ *
+ * @export
+ * @param {Object} build
+ * @param {Array} [dependencies=[]]
+ * @returns {Promise<Array>}
+ */
+export async function getDependencies(build, dependencies = []) {
+  if (!build.dependencies) {
+    return dependencies;
+  }
 
-  // Find recursively dependencies and stop on first loop detection:
-  //
+  // Retrieve current dependencies builds:
+  const builds = await Promise.all(_.map(build.dependencies, findByName));
+
+  // Retrieve builds dependencies:
+  const deps = await Promise.all(_.map(builds, b => getDependencies(b)));
+
+  return _.uniq([].concat(dependencies, _.map(builds, 'name'), ...deps));
+}
+
+/**
+ * Add a build dependency
+ *
+ * @export
+ * @param {any} _id
+ * @param {any} name
+ */
+export async function addDependency(node, name) {
+  const [build, dep] = await Promise.all([
+    findByName(node),
+    findByName(name)
+  ]);
+
+  if (name === build.name) {
+    throw new Error('A build can not depends on itself');
+  }
+
+  const dependencies = await getDependencies(dep);
+
+  if (dependencies.indexOf(build.name) !== -1) {
+    console.log(`${name} deps: ${dependencies} with ${build.name}`);
+    throw new Error('Cyclic dependency detected');
+  }
+
+  build.dependencies = _.uniq([].concat(build.dependencies || [], name));
+
+  await insertOrUpdate(build);
+
+  return build;
 }
 
 export default {
+  addDependency,
   findByName,
   get,
   insertOrUpdate,
--- src/web/api/builds/index.js
+++ src/web/api/builds/index.js
@@ -3,7 +3,7 @@ import { Router as createRouter } from 'express';
 import builds from '../../../models/builds';
 
 /**
- * Return a
+ * Return a specific build
  *
  * @export
  * @param {any} req
@@ -28,6 +28,31 @@ export async function getBuild(req, res, next) {
 }
 
 /**
+ * Add a dependency to a build
+ *
+ * @export
+ * @param {any} req
+ * @param {any} res
+ * @param {any} next
+ */
+export async function addDependency(req, res, next) {
+  try {
+    const build = await builds.addDependency(req.params.node_name, req.params.dependency_name);
+
+    if (build === null) {
+      return res.status(404).json({
+        code: 404,
+        message: 'Build not found'
+      });
+    }
+
+    res.json(build);
+  } catch (err) {
+    next(err);
+  }
+}
+
+/**
  * Controllers registration function
  *
  * @export
@@ -37,6 +62,7 @@ export default function register() {
   const router = createRouter();
 
   router.get('/:name', getBuild);
+  router.post('/:node_name/depends/:dependency_name', addDependency);
 
   return router;
 }
