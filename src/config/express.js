adb323610c478e8fc05d37efcc31dba52ccd7493
Gilles <gilles.rasigade+github@gmail.com>
2017-09-10 18:11:47 +0200

    Add CORS http verbs

--- src/config/express.js
+++ src/config/express.js
@@ -59,6 +59,7 @@ export function configure(app) {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
   });
 
